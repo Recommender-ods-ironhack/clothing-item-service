fd849be77095f3a0e307dbdd00dac5981ddfe4fc
Gab Caser <caserogabriela@gmail.com>
2025-05-22 11:38:25 +0200

    Initial commit

---
+++ src/main/java/com/recommender/clothing_item_service/ClothingItemServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recommender.clothing_item_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recommender/clothing_item_service/ClothingItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommender.clothing_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
