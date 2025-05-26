582170f8c8c98b48350682496fbc6b3b88ec516a
Gab Caser <caserogabriela@gmail.com>
2025-05-26 09:57:52 +0200

    add exception

--- src/main/java/com/recommender/clothing_item_service/exceptions/ItemNotFoundException.java
+++ src/main/java/com/recommender/clothing_item_service/exceptions/ItemNotFoundException.java
@@ -1,4 +1,8 @@
 package com.recommender.clothing_item_service.exceptions;
 
-public class ItemNotFoundException {
+public class ItemNotFoundException extends RuntimeException{
+
+     public ItemNotFoundException(Long id) {
+            super("Clothing item not found with ID: " + id);
+     }
 }
